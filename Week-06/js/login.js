258dee14a300abce9222b3fba78229e5e0256991
Bruno <piaggiobruno91@gmail.com>
2023-04-25 15:37:47 -0300

    BaSP-M2023:W06-login-correct

--- Week-06/js/login.js
+++ Week-06/js/login.js
@@ -1,56 +1,59 @@
-var box = document.getElementById("box");
-var EmailInput = document.getElementById("Email");
-var PasswordInput = document.getElementById("Password");
-var LoginButton = document.getElementById("login-button");
-
-
-function validateEmail(x){
-var Email = EmailInput.value;
-    if (Email.indexOf('@') === -1 || Email.indexOf('-') === -1){
-        EmailInput.style.border ='1px solid red';
-        x.preventDefault();
-        return false;
-    }
-    EmailInput.style.border ='2px solid green';
-    return true;
+window.onload = function (){
+     var emailLogin = document.getElementById("Email");
+     var emailErrorMsg = document.getElementById("emailErrorMsg");
+     var email = "example@email.com";
+     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+     var emailValid = false
+     if (email.match(emailExpression)) {
+         emailValid = true;
+     } else {
+         emailValid = false;
+     }
+     emailLogin.addEventListener('blur', function () {
+         var email = emailLogin.value;
+         for (var i = 0; i < email.length; i++) {
+         }
+         if (email.length < 9) {
+             emailErrorMsg.classList.remove("correct");
+             emailErrorMsg.classList.add("error");
+             emailErrorMsg.textContent = "email format is not valid";
+         }
+     });
+     emailLogin.onfocus = function () {
+         emailErrorMsg.classList.remove("error");
+         emailErrorMsg.classList.add("correct");
+     };
+     // password
+     var passwordLogin = document.getElementById("Password");
+     var passwordErrorMsg = document.getElementById("passwordErrorMsg");
+     passwordLogin.addEventListener('blur', function () {
+        var password = passwordLogin.value;
+        var hasBigLetter = false;
+        var hasSmallLetter = false;
+        var hasNumber = false;
+        for (var i = 0; i < password.length; i++) {
+             var char = password.charAt(i);
+             if (char >= "0" && char <= "9") {
+                hasNumber = true;
+             } else if (char === char.toUpperCase()) {
+                 hasBigLetter = true;
+             } else if (char === char.toLowerCase()) {
+                 hasSmallLetter = true;
+             }
+         }
+         if (!hasBigLetter || !hasSmallLetter || !hasNumber) {
+             passwordErrorMsg.classList.remove("correct");
+             passwordErrorMsg.classList.add("error");
+             passwordErrorMsg.textContent = "this field needs at least one capital letter, one small letter and one number.";
+         }
+         if (password.length < 5) {
+             passwordErrorMsg.classList.remove("correct");
+             passwordErrorMsg.classList.add("error");
+             passwordErrorMsg.textContent = "this field needs at least 8 letters";
+             }
+     });
+    passwordLogin.onfocus = function () {
+    passwordErrorMsg.classList.remove("error");
+         passwordErrorMsg.classList.add("correct");
+    };
 }
-function validatePassword (x) {
-    var Password = PasswordInput.value;
-    var Letter = false;
-    var numbers = false;
-
-    for (var i = 0; i < PasswordInput.length; i++) {
-        if (isNaN (Password.charAt(i))) {
-            Letter = true;
-        } else {
-            numbers = true;
-        }
-
-    }
-    if(Password.length < 8 || !(Letter && numbers)){
-        PasswordInput.style.border = "1px solid green";
-        x.preventDefault();
-        return false;
-    }
-    PasswordInput.style.border = "1px solid green";
-    return true;
-}
-
-function validatebox (x) {
-    var validEmail = validateEmail(x);
-    var validPassword = validatePassword(x);
-
-    if(!validEmail){
-        alert("Invalid Email")
-    }
-    if(!validPassword){
-        alert("Invalid password")
-    }
-    if(!validEmail && validPassword){
-        alert("Email: " + EmailInput.value + "\n Password: " + PasswordInput.value );
-    }
-}
-
-EmailInput.addEventListener("blur", validateEmail);
-PasswordInput.addEventListener("blur",validatePassword);
-LoginButton.addEventListener("blur", validatebox);
