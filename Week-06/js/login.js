bb79cf8b21357396c87ab0a27f6709b333a0c7b4
Bruno <piaggiobruno91@gmail.com>
2023-04-25 22:44:20 -0300

    BaSP-M2023:W06-signup-loginup

--- Week-06/js/login.js
+++ Week-06/js/login.js
@@ -3,14 +3,13 @@ window.onload = function (){
      var emailErrorMsg = document.getElementById("emailErrorMsg");
      var email = "example@email.com";
      var emailValid = isValidEmail(email);
+
     function isValidEmail(email) {
         var atIndex = email.indexOf('@');
         var dotIndex = email.lastIndexOf('.');
-        // Verifica si la dirección de correo electrónico contiene una "@" y un "."
         if (atIndex < 1 || dotIndex < atIndex + 2 || dotIndex + 2 >= email.length) {
           return false;
         }
-        // Verifica si la dirección de correo electrónico tiene más de 2 caracteres después del último punto
         if (email.slice(dotIndex + 1).length < 2) {
           return false;
         }
@@ -30,37 +29,40 @@ window.onload = function (){
          emailErrorMsg.classList.remove("error");
          emailErrorMsg.classList.add("correct");
      };
+
      // password
-     var passwordLogin = document.getElementById("Password");
-     var passwordErrorMsg = document.getElementById("passwordErrorMsg");
-     passwordLogin.addEventListener('blur', function () {
-        var password = passwordLogin.value;
-        var hasBigLetter = false;
-        var hasSmallLetter = false;
-        var hasNumber = false;
-        for (var i = 0; i < password.length; i++) {
-             var char = password.charAt(i);
-             if (char >= "0" && char <= "9") {
-                hasNumber = true;
-             } else if (char === char.toUpperCase()) {
-                 hasBigLetter = true;
-             } else if (char === char.toLowerCase()) {
-                 hasSmallLetter = true;
-             }
-         }
-         if (!hasBigLetter || !hasSmallLetter || !hasNumber) {
-             passwordErrorMsg.classList.remove("correct");
-             passwordErrorMsg.classList.add("error");
-             passwordErrorMsg.textContent = "this field needs at least one capital letter, one small letter and one number.";
-         }
-         if (password.length < 5) {
-             passwordErrorMsg.classList.remove("correct");
-             passwordErrorMsg.classList.add("error");
-             passwordErrorMsg.textContent = "this field needs at least 8 letters";
-             }
+    var passwordLogin = document.getElementById("Password");
+    var passwordErrorMsg = document.getElementById("passwordErrorMsg");
+    passwordLogin.addEventListener('blur', function () {
+    var password = passwordLogin.value;
+    var hasBigLetter = false;
+    var hasSmallLetter = false;
+    var hasNumber = false;
+    for (var i = 0; i < password.length; i++) {
+            var char = password.charAt(i);
+            if (char >= "0" && char <= "9") {
+            hasNumber = true;
+            } else if (char === char.toUpperCase()) {
+                hasBigLetter = true;
+            } else if (char === char.toLowerCase()) {
+                hasSmallLetter = true;
+            }
+        }
+        if (!hasBigLetter || !hasSmallLetter || !hasNumber) {
+            passwordErrorMsg.classList.remove("correct");
+            passwordErrorMsg.classList.add("error");
+            passwordErrorMsg.textContent = "this field needs at least one capital letter, one small letter and one number.";
+        }
+        if (password.length < 5) {
+            passwordErrorMsg.classList.remove("correct");
+            passwordErrorMsg.classList.add("error");
+            passwordErrorMsg.textContent = "this field needs at least 8 letters";
+            }
      });
-    passwordLogin.onfocus = function () {
-    passwordErrorMsg.classList.remove("error");
-         passwordErrorMsg.classList.add("correct");
+        passwordLogin.onfocus = function () {
+        passwordErrorMsg.classList.remove("error");
+        passwordErrorMsg.classList.add("correct");
     };
+
+
 }
